10ca75302db4d178065eac8d92c6293566109612
dkoqwekl312 <devacc11011@icloud.com>
2025-04-11 15:51:46 +0900

    로고 수정

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
 export default function Logo(){
-    return <Link className={'text-primary font-bold'} href='/'>The Soul</Link>
+    return <Link className={'text-base-content font-bold text-lg'} href='/'>The Soul</Link>
 
 }
