afc859ceae1a609af0213898bcf78a0d8137354c
devacc11011 <devacc11011@gmail.com>
2025-04-15 13:17:24 +0900

    깃헙 로고 수정

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import GitHubImage from "../../public/github-mark-white.svg";
 
 export default function Footer() {
     return (
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center my-3">
             <a href='http://github.com/devacc11011'>
-                <Image src='/github-mark.png' alt='github logo' width='24' height='24'
-                className='inline-block mx-1'/>
-                <span className='text-gray-700'>Github</span>
+                <Image src={GitHubImage} alt='github logo' width='24' height='24'
+                       className='inline-block mx-1 fill-error'/>
+                <span className='text-neutral-content'>Github</span>
             </a>
         </footer>
     )
